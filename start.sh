17af9df3bf3978bafda5cb8cf4e337c63470e779
Prince94163 <128693609+Prince94163@users.noreply.github.com>
2024-07-19 14:38:55 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+pip3 install -U -r requirements.txt
+pip[ install sudo
+sudo apt update
+sudo add-apt-repository universe
+sudo apt update
+sudo apt install ffmpeg
+echo "Starting Bot...."
+python3 video.py
